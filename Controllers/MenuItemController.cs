64eb5822d77909107b7a6f71748e391fa8dbbe30
Vimal Adhikari 849367 <6286140-vimal_adhikari@users.noreply.gitlab.com>
2020-10-30 12:18:07 +0530

    practice check added

---
+++ Controllers/MenuItemController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dockersampleconfig.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<MenuItem> Get()
+        {
+            return new List<MenuItem> {new MenuItem()
+                {
+                    Id = 1,
+                    Name = "Chicken"
+                    
+                },
+            new MenuItem()
+                {
+                    Id = 2,
+                    Name = "Soup"
+                   
+                }};
+        }
+    }
+}
+
+
+
